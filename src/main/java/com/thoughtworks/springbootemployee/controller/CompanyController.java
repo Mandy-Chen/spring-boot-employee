56d1a68b9460399d948acd898113c601ba542920
Austin <497133212@qq.com>
2020-07-28 17:50:56 +0800

    [Company API][Austin] refactor: modify Company and CompanyController

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +19,26 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAllCompanies() {
         List<Company> companies = new ArrayList<>();
-        companies.add(new Company(1, 1, 2));
-        companies.add(new Company(2, 1, 2));
-        companies.add(new Company(3,2,5));
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        companies.add(new Company(1,"alibaba",200, employees));
         return companies;
     }
 
     @GetMapping("/{companyId}")
     public Company getCompanyByCompanyId(@PathVariable int companyId) {
         Map<Integer, Company> companyMap = new HashMap<>();
-        companyMap.put(1, new Company(1, 1, 2));
-        companyMap.put(2, new Company(2, 1, 2));
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(4, "alibaba1", 20, "male", 6000));
+        employees.add(new Employee(6, "alibaba3", 19, "male", 8000));
+        employees.add(new Employee(11, "tengxun2", 19, "female", 7000));
+        companyMap.put(1, new Company(1, "alibaba", 100, employees));
         return companyMap.get(companyId);
     }
+
+
 
 
 }
