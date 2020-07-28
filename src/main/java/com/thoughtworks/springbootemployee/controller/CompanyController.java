1e4c5e0bc64549614542b4de82a4561340fa49f9
Austin <497133212@qq.com>
2020-07-28 17:27:00 +0800

    [Company API][Austin] feature: add obtain a certain specific company

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,11 +2,14 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/companies")
@@ -19,6 +22,14 @@ public class CompanyController {
         companies.add(new Company(2, 1, 2));
         companies.add(new Company(3,2,5));
         return companies;
+    }
+
+    @GetMapping("/{companyId}")
+    public Company getCompanyByCompanyId(@PathVariable int companyId) {
+        Map<Integer, Company> companyMap = new HashMap<>();
+        companyMap.put(1, new Company(1, 1, 2));
+        companyMap.put(2, new Company(2, 1, 2));
+        return companyMap.get(companyId);
     }
 
 
