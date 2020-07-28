6c1057ef8ec111410acef21b8bba974860e103e9
Austin <497133212@qq.com>
2020-07-28 21:08:03 +0800

    [Company API][Austin] feature: add CURD

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,17 +2,16 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.Util.DataBase;
 import com.thoughtworks.springbootemployee.model.Company;
+
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/companies")
@@ -21,14 +20,9 @@ public class CompanyController {
     @Autowired
     DataBase dataBase;
 
-    @GetMapping
-    public List<Company> getAllCompanies() {
-        return dataBase.getDataBase();
-    }
-
     @GetMapping("/{companyId}")
     public Company getCompanyByCompanyId(@PathVariable int companyId) {
-        List<Company> companies = dataBase.getDataBase();
+        List<Company> companies = dataBase.getCompanies();
         for(Company company : companies) {
             if(company.getCompanyId() == companyId) {
                 return company;
@@ -39,7 +33,7 @@ public class CompanyController {
 
     @GetMapping("/{companyId}/employees")
     public List<Employee> getAllEmployees(@PathVariable int companyId) {
-        List<Company> companies = dataBase.getDataBase();
+        List<Company> companies = dataBase.getCompanies();
         for(Company company : companies) {
             if(company.getCompanyId() == companyId) {
                 return company.getEmployees();
@@ -48,8 +42,49 @@ public class CompanyController {
         return null;
     }
 
+    @GetMapping
+    public List<Company> getPaginateCompanies(@RequestParam Integer page, @RequestParam Integer pageSize) {
+        if(page != null && pageSize != null) {
+            List<Company> companies = dataBase.getCompanies();
+            return companies.stream().filter(company ->
+                    companies.indexOf(company) >= page - 1 && companies.indexOf(company) <= pageSize - 1
+            ).collect(Collectors.toList());
+        }
+        return null;
+    }
 
 
+    @PostMapping
+    public void addCompany(@RequestBody Company company) {
+        List<Company> companies = dataBase.getCompanies();
+        companies.add(company);
+        dataBase.setCompanies(companies);
+    }
 
+    @PutMapping("/{companyId}")
+    public void updateCompany(@RequestBody Company companyInfo) {
+        List<Company> companies = dataBase.getCompanies();
+        for(Company company : companies) {
+            if(company.getCompanyId() == companyInfo.getCompanyId()) {
+                company.setCompanyId(companyInfo.getCompanyId());
+                company.setCompanyName(companyInfo.getCompanyName());
+                company.setEmployees(companyInfo.getEmployees());
+                company.setEmployeesNumber(companyInfo.getEmployeesNumber());
+                companies.add(company);
+                dataBase.setCompanies(companies);
+            }
+        }
+    }
 
+    @DeleteMapping("/{companyId}")
+    public void deleteCompany(@PathVariable int companyId) {
+        List<Company> companies = dataBase.getCompanies();
+        for(int i=0;i<companies.size();i++) {
+            if(companies.get(i).getCompanyId() == companyId) {
+                companies.remove(i);
+                i--;
+                dataBase.setCompanies(companies);
+            }
+        }
+    }
 }
