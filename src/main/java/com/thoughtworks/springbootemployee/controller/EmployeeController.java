c70e28ad6b7a8d44a830aa07ab76bcc5fbd23c23
Austin <497133212@qq.com>
2020-07-29 21:55:59 +0800

    [Company API][Austin Huang | Mandy chen] feature: implement EmployeeController function

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -3,41 +3,41 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-
     @Autowired
     EmployeeService employeeService;
 
     @GetMapping
-    public List<Employee> getEmployees(@RequestParam(required = false, defaultValue = "0") int pageNum, @RequestParam(required = false, defaultValue = "0") int pageSize, @RequestParam(required = false, defaultValue = "0") String gender) {
-        return null;
+    public Page<Employee> getEmployees(@RequestParam(required = false, defaultValue = "0") int page, @RequestParam(required = false, defaultValue = "0") int pageSize, @RequestParam(required = false, defaultValue = "0") String gender) {
+        return employeeService.getAllEmployees(page, pageSize);
     }
 
-
     @GetMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
     public Employee getEmployeeById(@PathVariable int employeeId) {
         return employeeService.getEmployeeById(employeeId);
     }
 
-
-
     @PostMapping
+    @ResponseStatus(HttpStatus.OK)
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
     @PutMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
     public Employee updateEmployee(@PathVariable int employeeId, @RequestBody Employee employee) {
         return employeeService.updateEmployee(employeeId, employee);
     }
 
     @DeleteMapping("{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteEmployeeById(@PathVariable int employeeId) {
         employeeService.deleteEmployeeById(employeeId);
     }
