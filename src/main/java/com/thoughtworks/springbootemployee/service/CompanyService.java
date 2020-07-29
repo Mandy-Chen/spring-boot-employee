725134540ef0966689d116063172409e6f086184
Austin <497133212@qq.com>
2020-07-29 14:29:25 +0800

    [Company API][Austin Huang | Mandy chen] feature: implement obtain company list

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -1,11 +1,18 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.Util.DataBase;
 import com.thoughtworks.springbootemployee.model.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CompanyService {
+
+
     public List<Company> findAllCompanies() {
-        return null;
+        DataBase dataBase = new DataBase();
+        return dataBase.getCompanies();
     }
 }
