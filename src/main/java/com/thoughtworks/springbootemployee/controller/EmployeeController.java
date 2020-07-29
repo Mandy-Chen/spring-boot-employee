06238a8802fcda278c05ecdf39ba1dcfb7be883a
Austin <497133212@qq.com>
2020-07-29 19:27:17 +0800

    [Company API][Austin Huang | Mandy chen] refactor: refactor application.yml and EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,63 +1,44 @@
 package com.thoughtworks.springbootemployee.controller;
 
-import com.thoughtworks.springbootemployee.Util.DataBase;
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    @Autowired
-    DataBase dataBase;
 
-    @GetMapping("/{employeeId}")
-    public Employee getEmployeeById(@PathVariable int employeeId) {
-        List<Employee> employees = dataBase.getEmployees();
-        return employees.stream().filter(employee -> employee.getId() == employeeId).findFirst().get();
-    }
+    @Autowired
+    EmployeeService employeeService;
 
     @GetMapping
     public List<Employee> getEmployees(@RequestParam(required = false, defaultValue = "0") int pageNum, @RequestParam(required = false, defaultValue = "0") int pageSize, @RequestParam(required = false, defaultValue = "0") String gender) {
-        List<Employee> employees = dataBase.getEmployees();
-        if (!"0".equals(gender))
-            return employees.stream().filter(employee -> gender.equals(employee.getGender())).collect(Collectors.toList());
-        if (0 == pageNum && 0 == pageSize) {
-            return employees;
-        } else {
-            List<Employee> employeesPage = employees.stream().filter(employee -> {
-                int index = employees.indexOf(employee);
-                return index >= pageNum * pageSize - pageSize && index <= pageNum * pageSize - 1;
-            }).collect(Collectors.toList());
-            return employeesPage;
-        }
+        return null;
     }
 
+
+    @GetMapping("/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+
+
     @PostMapping
-    public void addEmployee(@RequestBody Employee employee) {
-        List<Employee> employees = dataBase.getEmployees();
-        employees.add(employee);
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
     }
 
     @PutMapping("/{employeeId}")
-    public Employee updateEmployeeInfo(@PathVariable int employeeId, @RequestBody Employee changedEmployee) {
-        List<Employee> employees = dataBase.getEmployees();
-        Employee oldEmployee = employees.stream().filter(employee -> employeeId == employee.getId()).findFirst().get();
-        if (oldEmployee != null) {
-            employees.remove(oldEmployee);
-            employees.add(changedEmployee);
-        }
-        return changedEmployee;
+    public Employee updateEmployee(@PathVariable int employeeId, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(employeeId, employee);
     }
 
     @DeleteMapping("{employeeId}")
-    public void deleteEmployeeByEmployeeId(@PathVariable int employeeId) {
-        List<Employee> employees = dataBase.getEmployees();
-        Employee employee = employees.stream().filter(tempEmployee -> employeeId == tempEmployee.getId()).findFirst().get();
-        employees.remove(employee);
+    public void deleteEmployeeById(@PathVariable int employeeId) {
+        employeeService.deleteEmployeeById(employeeId);
     }
 }
