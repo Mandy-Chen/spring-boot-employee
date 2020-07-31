e73e08514fc3ac32fdd18e6b0f29d9e9f3a9728a
497133212 <497133212@qq.com>
2020-07-31 18:13:07 +0800

    go home

---
+++ src/test/java/com/thoughtworks/springbootemployee/mapper/CompanyMapperTest.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.springbootemployee.mapper;
+
+import com.thoughtworks.springbootemployee.dto.CompanyRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.model.Company;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CompanyMapperTest {
+    @Test
+    void should_get_company_when_mapper_given_company_request() {
+        //given
+        CompanyMapper companyMapper = new CompanyMapper();
+        CompanyRequest companyRequest = new CompanyRequest(0, "oocl", 100, emptyList());
+        //when
+        Company company = companyMapper.toCompany(companyRequest);
+        //then
+        assertEquals(companyRequest.getCompanyId(), company.getCompanyId());
+        assertEquals(companyRequest.getCompanyName(), company.getCompanyName());
+        assertEquals(companyRequest.getEmployees(), company.getEmployees());
+
+    }
+}
