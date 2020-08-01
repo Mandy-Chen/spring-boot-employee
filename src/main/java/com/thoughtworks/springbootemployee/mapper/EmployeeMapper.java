cf9c413a1ae09770f7e86fb86f5eed26255ddb4d
Mandy <chenjiafeng16@126.com>
2020-08-01 21:57:09 +0800

    [dto][mandy chen] test : add should_get_employee_when_mapper_given_employee_response test

--- src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.stereotype.Component;
 
@@ -12,5 +13,12 @@ public class EmployeeMapper {
                 employeeRequest.getGender(),
                 employeeRequest.getSalary(),
                 employeeRequest.getCompanyId());
+    }
+    public Employee toEmployee(EmployeeResponse employeeResponse) {
+        return new Employee(employeeResponse.getId(), employeeResponse.getName(),
+                employeeResponse.getAge(),
+                employeeResponse.getGender(),
+                employeeResponse.getSalary(),
+                employeeResponse.getCompanyId());
     }
 }
--- src/test/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapperTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,22 @@ public class EmployeeMapperTest {
         assertEquals(employeeRequest.getGender(), employee.getGender());
         assertEquals(employeeRequest.getName(), employee.getName());
         assertEquals(employeeRequest.getSalary(), employee.getSalary());
+
+    }
+    @Test
+    void should_get_employee_when_mapper_given_employee_response() {
+        //given
+        EmployeeMapper employeeMapper = new EmployeeMapper();
+        EmployeeResponse employeeResponse = new EmployeeResponse(0, "chen", 18, "female", 9999, 1);
+        //when
+        Employee employee = employeeMapper.toEmployee(employeeResponse);
+        //then
+        assertEquals(employeeResponse.getId(), employee.getId());
+        assertEquals(employeeResponse.getAge(), employee.getAge());
+        assertEquals(employeeResponse.getCompanyId(), employee.getCompanyId());
+        assertEquals(employeeResponse.getGender(), employee.getGender());
+        assertEquals(employeeResponse.getName(), employee.getName());
+        assertEquals(employeeResponse.getSalary(), employee.getSalary());
 
     }
 }
